6de1eeb111a6fb7807deb66397b49b6310482bc1
ahnafi <devsulthon@gmail.com>
2025-01-13 17:33:38 +0700

    test (script.js) : get api

--- script/script.js
+++ script/script.js
@@ -1,1 +1,27 @@
+const edition = "quran-uthmani";
 
+const Endpoint = {
+    listSurah: "https://api.alquran.cloud/v1/surah",
+    detailSurah: "https://api.alquran.cloud/v1/surah/",
+    // Juz : "http://api.alquran.cloud/v1/juz/"
+}
+
+async function getListSurah() {
+    try {
+        const response = await $.get(Endpoint.listSurah);
+        return response.data;
+    } catch (error) {
+        alert(`error Fetching data ${error.message}`)
+    }
+}
+
+getListSurah().then(function (params) {
+    params.forEach(function (items) {
+        console.log(items)
+        $("body").append(`
+                <p>
+                 Surah : ${items.name} <br>
+                 English Name : ${items.englishName} 
+                </p>`)
+    });
+});
