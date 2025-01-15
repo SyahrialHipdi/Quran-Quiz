9dbb1510845eb90af0eacb4d65aff4d5d7efb92b
SyahrialHipdi <MHsyahrial6@gmail.com>
2025-01-15 16:37:23 +0700

    selesai

--- script/script.js
+++ script/script.js
@@ -1,14 +1,11 @@
 // script.js
-function openPopup(title, subtitle, arabic, ayahCount) {
-    document.getElementById('popup-title').innerText = title;
-    document.getElementById('popup-subtitle').innerText = "Masukan range ayat yang akan dihafal";
-    document.getElementById('popup-arabic').innerText = arabic;
-    document.getElementById('popup-ayah-count').innerText = ayahCount;
-  
-    document.getElementById('popup').style.display = 'flex';
-  }
-  
-  function closePopup() {
-    document.getElementById('popup').style.display = 'none';
-  }
-  
+function openPopup(title) {
+  document.getElementById('popup-title').innerText = title;
+  document.getElementById('popup-subtitle').innerText = "Masukan range ayat yang akan dihafal";
+
+  document.getElementById('popup').style.display = 'flex';
+}
+
+function closePopup() {
+  document.getElementById('popup').style.display = 'none';
+}
