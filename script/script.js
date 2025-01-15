099477292c27a5e271287c39a6e0c7e1e7e7f4a1
ahnafi <devsulthon@gmail.com>
2025-01-15 17:45:12 +0700

    add (script.js) : get surah function

--- script/script.js
+++ script/script.js
@@ -15,13 +15,42 @@ async function getListSurah() {
     }
 }
 
-getListSurah().then(function (params) {
-    params.forEach(function (items) {
-        console.log(items)
-        $("body").append(`
-                <p>
-                 Surah : ${items.name} <br>
-                 English Name : ${items.englishName} 
-                </p>`)
-    });
-});
+/*
+* surah int between 1 and 114
+* from int default 0 (ayah 1)
+* to int default null cannot < 0
+*/
+async function getSurah(surah = 1, from = 0, to = null) {
+    try {
+        if (to <= 0) to = 1;
+
+        if (surah < 1 || surah > 114) {
+            // Error handler
+            console.error("Surah number is out of range. It should be between 1 and 114.");
+            return;
+        }
+
+        const response = await $.get(Endpoint.detailSurah + `${surah}?offset=${from}&limit=${to ?? ""}`);
+        console.log(Endpoint.detailSurah + `${surah}?offset=${from}&limit=${to ?? ""}`);
+        console.log(response.data);
+
+        // const list = response.data.ayahs;
+        // list.forEach(function (params) {
+        //     $("body").append(`<p>${JSON.stringify(params)}</p>`);
+        // });
+        return response.data
+    } catch (error) {
+        // Error handler (error.responseJSON.data)
+        console.error("Error fetching data: ", error.responseJSON?.data || error.message);
+    }
+}
+
+
+getSurah(1, 2, 0)
+
+function randomAyah() {
+    const math = Math.random() * 3;
+    console.log(math)
+}
+
+randomAyah()
